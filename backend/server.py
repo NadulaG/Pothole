9bc868db8d7b5b4487292894c8f357301af3861c
NadulaG <nadula.gardiyehewa@gmail.com>
2025-11-08 21:27:06 -0500

    fix conflict

--- backend/server.py
+++ backend/server.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 from street_view import generate_folder
 from street_hazard_upload import upload_local_file_to_supabase
 from werkzeug.exceptions import BadRequest
-import os, re, json
-from flask_cors import CORS
+import os
 import re
 import json
 import threading
 from flask_cors import CORS
+import re
 
 load_dotenv()
 
